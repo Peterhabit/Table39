15cf2f68bbd1aa718a7cce5c39e2b954a3627534
kjw <lkk7209@naver.com>
2023-04-13 11:58:29 +0900

    Initial Commit

---
+++ Table39/ViewController.swift
@@ -0,0 +1,37 @@
+//
+//  ViewController.swift
+//  Table39
+//
+//  Created by 소프트웨어컴퓨터 on 2023/04/13.
+//
+
+import UIKit
+
+class ViewController: UIViewController, UITableViewDelegate, UITableViewDataSource {
+    
+    @IBOutlet weak var table: UITableView!
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 10
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = UITableViewCell.init(style: .subtitle, reuseIdentifier: "myCell")
+        cell.textLabel?.text = "\(indexPath.row)"
+        cell.detailTextLabel?.text = indexPath.description
+        cell.imageView?.image = UIImage(named: "plant.png")
+        return cell
+    }
+    
+    func numberOfSections(in tableView: UITableView) -> Int {
+        return 3
+    }
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        table.dataSource = self
+        table.delegate = self
+    }
+
+
+}
